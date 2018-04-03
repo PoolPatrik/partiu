5204ace2e1627a7776cc3b4412ae086d451a1459
pool patrik <poolelise@sou.faccat.br>
2018-04-03 21:16:17 +0000

    novo home

---
+++ src/js/newjs.js
@@ -0,0 +1,8 @@
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: false // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+  if (!window.jQuery) {
+        document.write("<script src='bin/jquery-2.1.1.min.js'><\/script>")
+      }
