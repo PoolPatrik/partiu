4709e7465c48973c0e9d85da40daf51cb6c9fe00
Guilherme Appel <guilhermeaappel@gmail.com>
2018-05-31 22:39:46 -0300

    Refatoração do projeto. Alterado telas e versão do Materialize.

--- src/js/newjs.js
+++
@@ -1,8 +0,0 @@
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    }
-  );
-  if (!window.jQuery) {
-        document.write("<script src='bin/jquery-2.1.1.min.js'><\/script>")
-      }
