9544e698807c20fc7b9ab006bb2077c1ae2ab347
Guilherme Appel <guilhermeaappel@gmail.com>
2018-06-10 23:11:27 -0300

    Adicionado página para oferecer caronas.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var homeRouter = require('./routes/home');
 var cadastroRouter = require('./routes/cadastro');
-
+var oferecerCaronaRouter = require('./routes/oferecer-carona');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 app.use('/cadastro', cadastroRouter);
+app.use('/oferecer-carona', oferecerCaronaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.post('/login', function(req, res){
     }
   
     if(!user){
-      return res.status(404).send();
+      return res.status(401).send();
     }
   
     res.redirect('/home');
