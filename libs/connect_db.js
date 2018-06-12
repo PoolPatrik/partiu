dccfde5be3c96840840272ff714fa1a3724cd21b
Guilherme Arui Appel <gappel@secullum.local>
2018-06-12 13:23:40 -0300

    Atualizada conexão com o banco de dados.

--- libs/connect_db.js
+++ libs/connect_db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const mongoConnect = function() { 
-    mongoose.connection.openUri('mongodb://localhost/partiu', (err, res) => {
+    mongoose.connection.openUri('mongodb://partiu:faccat123@ds257470.mlab.com:57470/partiu-faccat', (err, res) => {
         if (err) {
             console.log('Failed: ' + err);
         } else {
