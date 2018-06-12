c9dbd594cd7ee299dc0c5c7ce8704d7d7e8b7a39
Guilherme Appel <guilhermeaappel@gmail.com>
2018-06-12 18:56:50 -0300

    Atualizadas telas de oferecer e encontrar carona.

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var model = require('./../model/caronas');
 
 router.get('/', function(req, res, next) {
-  res.render('../views/pages/home.ejs');
+  //res.render('../views/pages/home.ejs');
+  model.find(null, function(err, caronas){
+    if (err) {
+      throw err;
+    }
+
+    res.render('../views/pages/home.ejs', {caronas: caronas});
+  });
 });
 
 module.exports = router;
